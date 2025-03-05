7f403a031638ffa3cbc8c7de4487a7350699f8ca
Geeth Anuradha <Geethanu1999@gmail.com>
2025-03-05 13:36:26 +0530

    some home page basic design add

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="border-t bg-gray-50">
+      <div className="wrapper flex flex-col items-center justify-between gap-4 p-5 text-center sm:flex-row">
+        <Link href="/" aria-label="Go to homepage">
+          <Image
+            src="/assets/images/logo.svg"
+            alt="Evently Logo"
+            width={128}
+            height={38}
+            priority
+          />
+        </Link>
+
+        <p className="text-gray-600">© 2025 Evently. All rights reserved.</p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,21 +1,44 @@
 import Link from "next/link";
 import Image from "next/image";
 import React from "react";
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { Button } from "../ui/button";
+import NavItems from "./NavItems";
+import Mobilenav from "./Mobilenav";
 
 const Header = () => {
   return (
-    <header className="w-full  border-0">
-      <div className="wrapper flex items-center justify-between"></div>
-      <Link href="/" className="w-36">
-        <Image
-          src="/assets/images/logo.svg"
-          width={128}
-          height={38}
-          alt="Evently logo"
-        />
-      </Link>
+    <header className="w-full border-b">
+      <div className="max-w-screen-xl mx-auto flex items-center justify-between px-4 py-2">
+        {/* Logo */}
+        <Link href="/" className="w-36">
+          <Image
+            src="/assets/images/logo.svg"
+            width={128}
+            height={38}
+            alt="Evently logo"
+          />
+        </Link>
 
-      <div className="flex w-32 justify-end  gap-3"></div>
+        <SignedIn>
+          <nav className="hidden md:flex gap-4 w-full max-w-xs">
+            <NavItems />
+          </nav>
+        </SignedIn>
+
+        {/* Login Button (for SignedOut users) */}
+        <div className="flex justify-end gap-2">
+          <SignedIn>
+            <UserButton afterSignOutUrl="/" />
+            <Mobilenav />
+          </SignedIn>
+          <SignedOut>
+            <Button asChild className="rounded-full" size="lg">
+              <Link href="/sign-in">Login</Link>
+            </Button>
+          </SignedOut>
+        </div>
+      </div>
     </header>
   );
 };
